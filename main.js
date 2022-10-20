03ceb4f56a3e90440471f6d84c0b0def51003011
mohak51 <72740889+mohak51@users.noreply.github.com>
2022-10-20 17:08:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function sr(){
+    window.location = "studyroom.html";
+}
+
+function st(){
+    window.location = "st.html";
+}
+
+function cs(){
+    window.location = "cs.html";
+}
+
+function dt(){
+    window. location = "dt.html";
+}
+
+function sc(){
+    window. location = "sc.html";
+}
